8d490566a2b8d847783b68ffe0305790004fc24e
huangkai <h1770360848@outlook.com>
2026-02-03 16:06:26 +0800

    feat(lab 4): exercise 1

--- labs/lab04/ex1.s
+++ labs/lab04/ex1.s
@@ -48,9 +48,11 @@ next_test:
 # }
 #
 pow:
-    # BEGIN PROLOGUE
-    # FIXME: Need to save the callee saved register(s)
-    # END PROLOGUE
+    # Prologue
+    addi sp, sp, -8
+    sw ra, 0(sp)    # store ra value on the stack
+    sw s0, 4(sp)    # store s0 value on the stack
+
     li s0, 1
 pow_loop:
     beq a1, zero, pow_end
@@ -59,9 +61,12 @@ pow_loop:
     j pow_loop
 pow_end:
     mv a0, s0
-    # BEGIN EPILOGUE
-    # FIXME: Need to restore the callee saved register(s)
-    # END EPILOGUE
+
+    # Epilogue
+    lw ra, 0(sp)    # restore ra value from the stack
+    lw s0, 4(sp)    # restore s0 value from the stack
+    addi sp, sp, 8
+
     jr ra
 
 # Increments the elements of an array in-place.
@@ -72,9 +77,10 @@ pow_end:
 # address as argument and increments the 32-bit value stored there.
 inc_arr:
     # BEGIN PROLOGUE
-    # FIXME: What other registers need to be saved?
-    addi sp, sp, -4
+    addi sp, sp, -12
     sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
     # END PROLOGUE
     mv s0, a0 # Copy start of array to saved register
     mv s1, a1 # Copy length of array to saved register
@@ -84,20 +90,24 @@ inc_arr_loop:
     slli t1, t0, 2 # Convert array index to byte offset
     add a0, s0, t1 # Add offset to start of array
     # Prepare to call helper_fn
-    #
-    # FIXME: Add code to preserve the value in t0 before we call helper_fn
+    addi sp, sp, -4
+    sw t0, 0(sp)
+
     # Also ask yourself this: why don't we need to preserve t1?
-    #
     jal ra helper_fn
-    # FIXME: Restore t0
+
+    lw t0, 0(sp)
+    addi sp, sp, 4
+
     # Finished call for helper_fn
     addi t0, t0, 1 # Increment counter
     j inc_arr_loop
 inc_arr_end:
     # BEGIN EPILOGUE
-    # FIXME: What other registers need to be restored?
     lw ra, 0(sp)
-    addi sp, sp, 4
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
     # END EPILOGUE
     jr ra
 
@@ -111,13 +121,17 @@ inc_arr_end:
 # as appropriate.
 helper_fn:
     # BEGIN PROLOGUE
-    # FIXME: YOUR CODE HERE
+    addi sp, sp, -8
+    sw ra, 0(sp)
+    sw s0, 4(sp)
     # END PROLOGUE
     lw t1, 0(a0)
     addi s0, t1, 1
     sw s0, 0(a0)
     # BEGIN EPILOGUE
-    # FIXME: YOUR CODE HERE
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    addi sp, sp, 8
     # END EPILOGUE
     jr ra
 
